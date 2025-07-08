292e6a120fb5bc3b236caa19a5bea7fbd23da142
shangi Shangstone <patrickshangstone22@gmail.com>
2025-07-08 12:24:36 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button type="subm

--- src/components/common/HeroSearch.tsx
+++ src/components/common/HeroSearch.tsx
@@ -147,7 +147,7 @@ export default function HeroSearch() {
               </DialogFooter>
             </DialogContent>
           </Dialog>
-          <Button type="submit" size="lg" className="h-11 rounded-full bg-[#F57900] hover:bg-[#F57900]/90 text-white">
+          <Button type="submit" size="lg" className="h-11 rounded-full bg-[#28235f] hover:bg-[#28235f]/90 text-white">
             <Search className="w-5 h-5 mr-0 md:mr-2" />
             <span className="hidden md:inline">Search</span>
           </Button>
