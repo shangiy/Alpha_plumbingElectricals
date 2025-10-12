e43f8ef4cbfecf7cc99e2bf5e74f57cb8094eb35
shangi Shangstone <patrickshangstone22@gmail.com>
2025-10-12 21:06:11 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ .idx/dev.nix
@@ -0,0 +1,12 @@
+{pkgs}: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_20
+  ];
+  idx.previews = {
+    web = {
+      command = ["npm", "run", "dev", "--", "--port", "$PORT", "--hostname", "0.0.0.0"];
+      manager = "web";
+    };
+  };
+}
