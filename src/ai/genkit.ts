491ecb80013e123d6c81462b7266ac4ef380c569
shangi Shangstone <patrickshangstone22@gmail.com>
2026-03-22 03:32:14 +0000

    stiil unsuccessful

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,7 @@
 import { genkit } from 'genkit';
-import { googleAI } from '@genkit-ai/googleai';
+import { googleAI } from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'gemini-1.5-flash',
+  model: 'googleai/gemini-1.5-flash',
 });
