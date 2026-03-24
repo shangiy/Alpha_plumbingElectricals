a450d2147ee959b06e19f8ada976fb2395fb17c3
shangi Shangstone <patrickshangstone22@gmail.com>
2026-03-24 23:11:03 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Cannot read prop

--- src/components/layout/ClientLayout.tsx
+++ src/components/layout/ClientLayout.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import * as React from 'react';
 import { useState } from 'react';
 import { usePathname } from 'next/navigation';
 import { Toaster } from '@/components/ui/toaster';
